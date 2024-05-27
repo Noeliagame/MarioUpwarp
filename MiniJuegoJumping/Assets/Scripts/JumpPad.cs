67508db5589c40f750e8ee1a9b618c69dd660206
Noeliagame <noeliayortizp@gmail.com>
2024-05-27 17:10:03 -0500

    Sound_Block
    Se agrego el sonido a los bloques dinámicos.

--- MiniJuegoJumping/Assets/Scripts/JumpPad.cs
+++ MiniJuegoJumping/Assets/Scripts/JumpPad.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     public float bounce = 5f;
+    public AudioSource BlockSound;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            BlockSound.Play();
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
     }
