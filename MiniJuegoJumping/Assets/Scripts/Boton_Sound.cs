559ad1949f224daba4d254ad299f777f02b19553
Noeliagame <noeliayortizp@gmail.com>
2024-05-27 18:11:57 -0500

    Creditos y soninido de botones
    Se agrega el menú de créditos y se coloca sonido a los botones.

---
+++ MiniJuegoJumping/Assets/Scripts/Boton_Sound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boton_Sound : MonoBehaviour
+{
+    public AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public Button Btn { get { return GetComponent<Button>(); } }
+    public AudioClip BotonSound;
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+
+        Btn.onClick.AddListener(PlaySound);
+    }
+
+    // Update is called once per frame
+    void PlaySound()
+    {
+        source.PlayOneShot (BotonSound);
+    }
+}
